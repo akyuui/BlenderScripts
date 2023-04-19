5bd021fd4f9cb2e1d0342153ff17cf2bb262b02e
akyuui <niikokoro@gmail.com>
2023-04-19 08:49:03 -0300

    first commit

---
+++ FixAnimationScales.py
@@ -0,0 +1,14 @@
+import bpy
+
+y_scale = 100
+
+try:
+    for action in bpy.data.actions:
+        for fcurve in action.fcurves:
+            if fcurve.data_path.endswith("location"):
+                for keyframe_point in fcurve.keyframe_points:
+                    keyframe_point.co.y *= y_scale
+                    keyframe_point.handle_left.y *= y_scale
+                    keyframe_point.handle_right.y *= y_scale
+except TypeError:
+    pass
